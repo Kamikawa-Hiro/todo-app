22759720392412725772f6f9807c163a1ebdd110
Kamikawa-Hiro <rkamikawauniv@gmail.com>
2025-06-25 09:55:23 +0900

    make complete and delete

--- todo.go
+++ todo.go
@@ -59,8 +59,51 @@ func ListTasks() {
 
 func CompleteTask(id int) {
 	panic("unimplemented")
+	found := false
+	tasks, err := loadTasks()
+
+	for i := range tasks {
+		if tasks[i].ID == targetID {
+			tasks[i].Done = true
+			found = true
+			break // 見つけたらループ終了
+		}
+	}
+	if !found {
+		return errors.New("task not found")
+	}
+
+	err := saveTasks(tasks)
+	if err != nil {
+		fmt.Println("Error saving tasks:", err)
+	} else {
+		fmt.Println("Tasks saved successfully!")
+	}
 }
 
 func DeleteTask(id int) {
 	panic("unimplemented")
+
+	found := false
+	newTasks := make([]Task, 0, len(tasks)) // 新しいスライスを作る
+	tasks, err := loadTasks()
+	
+	for _, task := range tasks {
+		if task.ID == targetID {
+			found = true
+			continue // この task はスキップ（削除）
+		}
+		newTasks = append(newTasks, task)
+	}
+
+	if !found {
+		return tasks, errors.New("task not found")
+	}
+
+	err := saveTasks(newTasks)
+	if err != nil {
+		fmt.Println("Error saving tasks:", err)
+	} else {
+		fmt.Println("Tasks saved successfully!")
+	}
 }
